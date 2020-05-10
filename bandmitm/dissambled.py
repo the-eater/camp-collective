210e3c47fa70126b1fe5bb3946977bdc404ed47d
eater <=@eater.me>
2020-05-10 12:49:56 +0200

    use loop to evade recursion

--- bandmitm/dissambled.py
+++ bandmitm/dissambled.py
@@ -1,6 +1,6 @@
 from hashlib import sha1
 import string
-from typing import Optional
+from typing import Optional, Any, List
 
 
 def to_base36(num: int) -> str:
@@ -49,7 +49,7 @@ class PoW:
     def __str__(self):
         assembled = f"{self.b}:{self.c}:{self.d}"
         if self.e is not None:
-            return f"{assembled}:${self.e}"
+            return f"{assembled}:{self.e}"
 
         return assembled
 
@@ -69,12 +69,7 @@ class PoW:
 def scramble_pow(r12: str, r13: int, r14: int, r15: str) -> str:
     if r14 == 0:
         return "noop"
-    r = [0 for k in range(20)]
-    r[12] = r12
-    r[13] = r13
-    r[14] = r14
-    r[15] = r15
-    r[13] = 0
+    r: List[Any] = [0 for k in range(20)]
     r[0] = sha1()
     r[1] = 0
     r[2] = 0
@@ -84,10 +79,15 @@ def scramble_pow(r12: str, r13: int, r14: int, r15: str) -> str:
     r[7] = 0
     r[8] = 0
     r[9] = 0
+    r[12] = r12
+    r[13] = r13
+    r[14] = r14
+    r[15] = r15
+    r[13] = 0
 
     def L_0x000e(r):
         if r[13] != 0:
-            return
+            return 'L_0x008b'
         r[3] = to_base36(r[2])
         r[4] = f"{r[12]}{r[15]}{r[3]}"
         r[0].update(r[4].encode('utf-8'))
@@ -95,112 +95,123 @@ def scramble_pow(r12: str, r13: int, r14: int, r15: str) -> str:
         r[0] = sha1()
         r[5] = 0
         r[6] = 0
-        L_0x0038(r)
+        return 'L_0x0038'
 
     def L_0x0038(r):
         r[7] = len(r[4])
         if r[5] >= r[7]:
-            L_0x007d(r)
-            return
+            return 'L_0x007d'
         r[7] = r[4][r[5]]
         r[8] = 8
         r[9] = 1
         if r[7] >= 0:
-            L_0x0044(r)
-            return
-        L_0x0042(r)
+            return 'L_0x0044'
+        return 'L_0x0042'
 
     def L_0x0042(r):
         r[9] = 0
-        L_0x0076(r)
+        return 'L_0x0076'
 
     def L_0x0044(r):
         r[7] = r[4][r[5]]
         if r[7] >= r[9]:
-            L_0x004b(r)
-            return
+            return 'L_0x004b'
         r[9] = 8
-        L_0x0076(r)
+        return 'L_0x0076'
 
     def L_0x004b(r):
         r[7] = r[4][r[5]]
         if r[7] >= 2:
-            L_0x0052(r)
-            return
+            return 'L_0x0052'
         r[9] = 7
-        L_0x0076(r)
+        return 'L_0x0076'
 
     def L_0x0052(r):
         r[7] = r[4][r[5]]
         if r[7] >= r[8]:
-            L_0x0058(r)
-            return
+            return 'L_0x0058'
         r[9] = 6
-        L_0x0076(r)
+        return 'L_0x0076'
 
     def L_0x0058(r):
         r[7] = r[4][r[5]]
         if r[7] >= 16:
-            L_0x0060(r)
-            return
+            return 'L_0x0060'
         r[9] = 5
-        L_0x0076(r)
+        return 'L_0x0076'
 
     def L_0x0060(r):
         r[7] = r[4][r[5]]
         if r[7] >= 32:
-            L_0x0068(r)
+            return 'L_0x0068'
+
         r[9] = 4
-        L_0x0076(r)
+        return 'L_0x0076'
 
     def L_0x0068(r):
         r[7] = r[4][r[5]]
         if r[7] >= 64:
-            L_0x0070(r)
-            return
+            return 'L_0x0070'
 
         r[9] = 3
-        L_0x0076(r)
+        return 'L_0x0076'
 
     def L_0x0070(r):
         r[7] = r[4][r[5]]
         if r[7] >= 128:
-            L_0x0042(r)
-            return
+            return 'L_0x0042'
 
-        L_0x0076(r)
+        return 'L_0x0076'
 
     def L_0x0076(r):
         r[6] = r[6] + r[9]
         if r[9] == r[8]:
-            L_0x007a(r)
-            return
+            return 'L_0x007a'
 
-        L_0x007d(r)
+        return 'L_0x007d'
 
     def L_0x007a(r):
         r[5] = r[5] + 1
-        L_0x0038(r)
+        return 'L_0x0038'
 
     def L_0x007d(r):
         if r[6] < r[14]:
-            L_0x0080(r)
-            return
+            return 'L_0x0080'
         r[13] = r[3]
-        L_0x0080(r)
+        return 'L_0x0080'
 
     def L_0x0080(r):
         r[2] = r[2] + 1
-        L_0x000e(r)
+        return 'L_0x000e'
 
     def L_0x0083(r):
         print("bandcamp: Unable to compute answer <exception probably goes here or whatever>")
 
+    calls = {
+        'L_0x000e': L_0x000e,
+        'L_0x0038': L_0x0038,
+        'L_0x0042': L_0x0042,
+        'L_0x0044': L_0x0044,
+        'L_0x004b': L_0x004b,
+        'L_0x0052': L_0x0052,
+        'L_0x0058': L_0x0058,
+        'L_0x0060': L_0x0060,
+        'L_0x0068': L_0x0068,
+        'L_0x0070': L_0x0070,
+        'L_0x0076': L_0x0076,
+        'L_0x007a': L_0x007a,
+        'L_0x007d': L_0x007d,
+        'L_0x0080': L_0x0080,
+        'L_0x0083': L_0x0083,
+    }
+
     try:
-        L_0x000e(r)
+        current = "L_0x000e"
+        while current != "L_0x008b":
+            current = calls[current](r)
     except Exception as e:
         print(e)
-        #L_0x0083(r)
+        # return 'L_0x0083'
 
     r13 = r[13]
     return r13
